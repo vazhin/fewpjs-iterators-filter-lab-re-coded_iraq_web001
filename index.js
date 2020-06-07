ec44e069e98c2d84b74ba1f3d22935fc1f59c4d6
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-06-07 21:03:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function findMatching(arr, string) {
   return arr.filter(element => element === string.toLowerCase() || element === string.toUpperCase() || element === string.charAt(0).toUpperCase() + string.slice(1))
 }
 
-function fuzzyMatch() {
+function fuzzyMatch(arr, string) {
 
 }
