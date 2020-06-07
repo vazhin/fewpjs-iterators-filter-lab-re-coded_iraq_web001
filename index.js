973aeb9b1e6e1bb515993f227e1462b1a36203ec
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-06-07 21:10:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(arr, string) {
+  return arr.filter(element => element === string.toLowerCase() || element === string.toUpperCase() || element === string.charAt(0).toUpperCase() + string.slice(1))
+}
+
+function fuzzyMatch(arr, string) {
+  return arr.filter(element => element.startsWith(string))
+}
+
+function matchName(arrOfObjects, string) {
+  return arrOfObjects.filter(obj => obj.name === string)
+}
