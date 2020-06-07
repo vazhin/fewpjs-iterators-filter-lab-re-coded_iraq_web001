91f45ab7b559669d77322994ecd53375fe8973dc
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-06-07 21:01:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution here
 function findMatching(arr, string) {
-  return arr.filter(element => element === string.toLowerCase() || element === string.toUpperCase())
+  return arr.filter(element => element === string.toLowerCase() || element === string.toUpperCase() || element === string.charAt(0).toUpperCase() + string.slice(1))
 }
